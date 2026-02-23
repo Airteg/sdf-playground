0e79f8f70c6125c659a7df3cd5861f832f15d155
Airteg <airteg@ukr.net>
2026-02-23 18:26:24 +0200

    feat: оновлення шейдерів, заміна шейдера circle на square та додавання нового шейдера square.sksl

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@ import { Skia, SkRuntimeEffect } from "@shopify/react-native-skia";
 import React from "react";
 import { ShaderCanvas } from "./src/ShaderCanvas";
 
-import shaderSource from "./src/shaders/circle.sksl";
+import shaderSource from "./src/shaders/square.sksl";
+// import shaderSource from "./src/shaders/circle.sksl";
 // import shaderSource from "./src/shaders/gradient.sksl";
 
 function assertEffect(effect: SkRuntimeEffect | null): SkRuntimeEffect {
